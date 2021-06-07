12db83155fd908a1a322503927a441a9530932e0
Leo Yang <leo19941227@gmail.com>
2021-06-07 14:30:08 +0800

    add role embedding

--- model/risk_model.py
+++ model/risk_model.py
@@ -7,6 +7,7 @@ from torch.nn.utils.rnn import pad_sequence
 from .pretrained import PretrainModel
 from .pooling import get_pooler
 
+NUM_ROLES = 5
 
 class Classifier(nn.Module):
     def __init__(
@@ -15,14 +16,19 @@ class Classifier(nn.Module):
         super().__init__()
         self.pretrained_model = PretrainModel(**pretrained_cfg)
         self.projector = nn.Linear(self.pretrained_model.output_size, project_size)
-        self.diag_pooler = get_pooler(project_size, pooler_cfg)
-        self.predictor = nn.Linear(self.diag_pooler.output_size, 1)
+        self.role_embedding = nn.Embedding(NUM_ROLES, project_size)
+        self.pooler = get_pooler(project_size, pooler_cfg)
+        self.predictor = nn.Linear(self.pooler.output_size, 1)
 
     def forward(self, diags, diags_len, roles, **kwargs):
-        embeddings = self.pretrained_model(diags, diags_len.device)
+        content_embeddings = self.pretrained_model(diags, diags_len.device)
+        content_embeddings = self.projector(content_embeddings)
+        role_embeddings = self.role_embedding(roles)
+        embeddings = content_embeddings + role_embeddings
+
         embeddings = embeddings.split(diags_len.tolist())
-        embeddings = pad_sequence(embeddings, batch_first=True)
-        embeddings = self.projector(embeddings)
-        pooled_embbedings = self.diag_pooler(embeddings, diags_len)
+        diags_embeddings = pad_sequence(embeddings, batch_first=True)
+
+        pooled_embbedings = self.pooler(diags_embeddings, diags_len)
         logits = self.predictor(pooled_embbedings).view(-1)
         return F.sigmoid(logits)
