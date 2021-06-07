815320eebbcdf94783643c7273ff08af1436c06b
Leo Yang <leo19941227@gmail.com>
2021-06-07 10:58:00 +0800

    add attentive pooling

--- model/pooling.py
+++ model/pooling.py
@@ -48,3 +48,40 @@ class BLSTM(nn.Module):
         )
         diag_embeding = diag_embeding.mean(1).mean(1)
         return diag_embeding
+
+
+class AttentivePooling(nn.Module):
+    def __init__(self, input_size, activation="ReLU", **kwargs):
+        super(AttentivePooling, self).__init__()
+        self.input_size = input_size
+        self.sap_layer = AttentivePoolingModule(input_size, activation)
+
+    @property
+    def output_size(self):
+        return self.input_size
+
+    def forward(self, feature_BxTxH, features_len):
+        device = feature_BxTxH.device
+        len_masks = torch.lt(
+            torch.arange(features_len.max()).unsqueeze(0).to(device),
+            features_len.unsqueeze(1),
+        )
+        sap_vec, _ = self.sap_layer(feature_BxTxH, len_masks)
+        return sap_vec
+
+
+class AttentivePoolingModule(nn.Module):
+    def __init__(self, input_size, activation="ReLU", **kwargs):
+        super(AttentivePoolingModule, self).__init__()
+        self.W_a = nn.Linear(input_size, input_size)
+        self.W = nn.Linear(input_size, 1)
+        self.act_fn = getattr(nn, activation)()
+        self.softmax = nn.functional.softmax
+
+    def forward(self, batch_rep, att_mask):
+        att_logits = self.W(self.act_fn(self.W_a(batch_rep))).squeeze(-1)
+        att_logits = att_mask + att_logits
+        att_w = self.softmax(att_logits, dim=-1).unsqueeze(-1)
+        utter_rep = torch.sum(batch_rep * att_w, dim=1)
+
+        return utter_rep, att_w
