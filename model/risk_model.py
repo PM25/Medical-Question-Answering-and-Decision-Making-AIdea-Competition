40a3361cebf13ca1b0cb93dbd44a6298b7cde9d7
Leo Yang <leo19941227@gmail.com>
2021-06-07 14:05:51 +0800

    rename pooler

--- model/risk_model.py
+++ model/risk_model.py
@@ -10,12 +10,12 @@ from .pooling import get_pooler
 
 class Classifier(nn.Module):
     def __init__(
-        self, pretrained_cfg: Dict, project_size: int, diag_pooler_cfg: Dict, **kwargs
+        self, pretrained_cfg: Dict, project_size: int, pooler_cfg: Dict, **kwargs
     ):
         super().__init__()
         self.pretrained_model = PretrainModel(**pretrained_cfg)
         self.projector = nn.Linear(self.pretrained_model.output_size, project_size)
-        self.diag_pooler = get_pooler(project_size, diag_pooler_cfg)
+        self.diag_pooler = get_pooler(project_size, pooler_cfg)
         self.predictor = nn.Linear(self.diag_pooler.output_size, 1)
 
     def forward(self, diags, diags_len, roles, **kwargs):
